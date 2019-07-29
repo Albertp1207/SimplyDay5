60ed7a7b788f7f002f44252202d37c448974cc4e
Albert <albertp1207@mail.ru>
2019-07-29 14:13:27 +0400

    corrected user validation

--- task1/utils/validateUser.js
+++ task1/utils/validateUser.js
@@ -1,10 +1,23 @@
-const validateUser = user => {
+const validateUser = user => {    
+    const types = {
+        username: "string",
+        gender : "string",
+        agree : "boolean",
+        password : "string",
+    }
+
+    for(key in user) {
+        if(typeof user[key] !== types[key]) {
+            return false;
+        }
+    }
+
     // 1st for input, 2st for api
     if(user.agree !== "on" && user.agree !== true) {
         return false;
     }
-    const value = Object.values(user)
-    const isUserValid = value.every(elem => {
+    const values = Object.values(user)
+    const isUserValid = values.every(elem => {
         if(typeof elem === "boolean"){
             return true;
         } 
